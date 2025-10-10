f9424e2a178ada3b6525bd53f17a5383cf14bb0e
Diego Barbosa Vasconcelos <diegovasconcelosb@gmail.com>
2025-10-10 10:46:09 -0300

    feat: adding swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors();
-
   app.setGlobalPrefix('glycemic-map-api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Glycemic Map API')
+    .setDescription('API para monitoramento e análise de níveis glicêmicos com geração automatizada de relatórios em PDF')
+    .setVersion('1.0')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/glycemic-map-api/swagger', app, document, {
+      customSiteTitle: 'Glycemic Map API',
+      customCss: '.swagger-ui .topbar { display: none }',
+  });
   
   await app.listen(3000);
   console.log('Application is running on: http://localhost:3000');
