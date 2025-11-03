e24a2f327112a555b721a3fdcec3629b15535377
Diego Barbosa Vasconcelos <diegovasconcelosb@gmail.com>
2025-11-03 16:28:39 -0300

    fix: making better use of decorator column declarations and changing

--- src/db/migrations/V1_create_glycemia_table.sql
+++ src/db/migrations/V1_create_glycemia_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE  IF NOT EXISTS glycemia (
     glycemia_id SERIAL PRIMARY KEY,
-    glycemia NUMERIC(5,2) NOT NULL,
+    glycemia NUMERIC(3) NOT NULL,
     result VARCHAR(50) NOT NULL,
     meal VARCHAR(50),
-    observation TEXT,
+    observation TEXT DEFAULT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NULL
 );
--- src/glycemia/entites/glycemia.entitie.ts
+++ src/glycemia/entites/glycemia.entitie.ts
@@ -6,25 +6,40 @@ export class Glycemia {
     @PrimaryGeneratedColumn({name: 'glycemia_id'})
     glycemiaId: number;
 
-    @Column({name: 'glycemia'})
+    @Column({
+        name: 'glycemia',
+        type: 'int',
+    })
     glycemia: number;
 
     @Column({
         name: 'result',
         type: 'enum',
-        enum: GlycemiaStatus
+        enum: GlycemiaStatus,
     })
     result: GlycemiaStatus;
 
-    @Column({name: 'meal'})
+    @Column({
+        name: 'meal',
+        type: 'varchar',
+    })
     meal: string;
 
-    @Column({name: 'observation'})
+    @Column({
+        name: 'observation',
+        type: 'varchar',
+    })
     observation: string;
 
-    @Column({name: 'created_at'})
+    @Column({
+        name: 'created_at',
+        type: 'timestamp',
+    })
     created_at: Date;
 
-    @Column({name: 'updated_at'})
+    @Column({
+        name: 'updated_at',
+        type: 'timestamp',
+    })
     updated_at: Date;
 }
